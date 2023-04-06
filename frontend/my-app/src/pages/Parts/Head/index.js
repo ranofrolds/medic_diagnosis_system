e1ccb40caa126b59d71b5925f4b835a593a8698d
Gabriel Biscaia <ra118928@uem.br>
2023-04-06 20:48:28 -0300

    adição de html no stomach e intimate

--- frontend/my-app/src/pages/Parts/Head/index.js
+++ frontend/my-app/src/pages/Parts/Head/index.js
@@ -1,13 +1,76 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../../../styles/style.css";
 
 export const Head = () => {
+  //Define um estado para armazenar os valores dos campos do formulário
+  const [formValues, setFormValues] = useState({
+    dor_de_cabeca: false,
+    nausea: false,
+    sensibilidade_a_luz: false,
+    tontura: false,
+    visao_turva: false,
+  });
+
+  // Função que atualiza o estado quando um campo é modificado
+  const handleInputChange = (event) => {
+    const { name, checked } = event.target;
+    setFormValues({ ...formValues, [name]: checked });
+  };
+
   return (
     <div>
       <h1>Sintomas na Cabeça</h1>
-      <Link to="/chest">
-        <button>Próximo</button>
-      </Link>
+      <form>
+        <label>
+          <input
+            type="checkbox"
+            name="dor_de_cabeca"
+            checked={formValues.dor_de_cabeca}
+            onChange={handleInputChange}
+          />
+          Dor de cabeça
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="nausea"
+            checked={formValues.nausea}
+            onChange={handleInputChange}
+          />
+          Náusea
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="sensibilidade_a_luz"
+            checked={formValues.sensibilidade_a_luz}
+            onChange={handleInputChange}
+          />
+          Sensibilidade a Luz
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="tontura"
+            checked={formValues.tontura}
+            onChange={handleInputChange}
+          />
+          Tontura
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="visao_turva"
+            checked={formValues.visao_turva}
+            onChange={handleInputChange}
+          />
+          Visão turva
+        </label>
+        <Link to="/chest">
+          <button type="submit">Próximo</button>
+        </Link>
+      </form>
     </div>
   );
 };
--- frontend/my-app/src/pages/Parts/Intimate/index.js
+++ frontend/my-app/src/pages/Parts/Intimate/index.js
@@ -1,13 +1,56 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../../../styles/style.css";
 
 export const Intimate = () => {
+  //Define um estado para armazenar os valores dos campos do formulário
+  const [formValues, setFormValues] = useState({
+    coceira_anal: false,
+    dor_ao_evacuar: false,
+    sangramento_ao_evacuar: false,
+  });
+
+  // Função que atualiza o estado quando um campo é modificado
+  const handleInputChange = (event) => {
+    const { name, checked } = event.target;
+    setFormValues({ ...formValues, [name]: checked });
+  };
+
   return (
     <div>
       <h1>Sintomas nas Regiões Íntimas</h1>
-      <Link to="/">
-        <button>Próximo</button>
-      </Link>
+      <form>
+        <label>
+          <input
+            type="checkbox"
+            name="coceira_anal"
+            checked={formValues.coceira_anal}
+            onChange={handleInputChange}
+          />
+          Coceira anal
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="dor_ao_evacuar"
+            checked={formValues.dor_ao_evacuar}
+            onChange={handleInputChange}
+          />
+          Dor ao evacuar
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="sangramento_ao_evacuar"
+            checked={formValues.sangramento_ao_evacuar}
+            onChange={handleInputChange}
+          />
+          Sangramento ao evacuar
+        </label>
+        <Link to="/">
+          <button type="submit">Próximo</button>
+        </Link>
+      </form>
     </div>
   );
 };
