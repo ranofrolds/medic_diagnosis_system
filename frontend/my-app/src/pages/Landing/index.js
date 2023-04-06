cfc2055342e8504db9fa7b44516dbf5fbbc718e6
Gabriel Biscaia <ra118928@uem.br>
2023-04-06 16:47:56 -0300

    adição LandingPage

---
+++ frontend/my-app/src/pages/Landing/index.js
@@ -0,0 +1,11 @@
+import "../../styles/style.css";
+
+export const Landing = () => {
+  return (
+    <div>
+      <h1>Bem-vindo</h1>
+      <button>Fazer consulta</button>
+      <a href="#">Consultar Pacientes</a>
+    </div>
+  );
+};
--- frontend/my-app/src/routes/index.js
+++ frontend/my-app/src/routes/index.js
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import { App } from "../App";
+import { Landing } from "../pages/Landing";
 
 export const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Landing />} />
       </Routes>
     </Router>
   );
