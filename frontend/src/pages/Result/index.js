c15e2211a2811078f0f4306737fda2fd84edb8a0
Gabriel Biscaia <ra118928@uem.br>
2023-04-06 23:09:02 -0300

    adição de texto no resultado final

--- frontend/src/pages/Result/index.js
+++ frontend/src/pages/Result/index.js
@@ -5,6 +5,12 @@ export const Result = () => {
   return (
     <div>
       <h1>Resultado</h1>
+      <p>
+        <b>
+          O resultado do protótipo é apenas informativo, o paciente deve
+          consultar um médico para obter um diagnóstico correto e preciso
+        </b>
+      </p>
       <Link to="/">
         <button>Voltar ao menu</button>
       </Link>
