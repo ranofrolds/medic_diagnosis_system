2ec6965fcaa2da3f50acaf7df796b3d69e858961
Gabriel Biscaia <ra118928@uem.br>
2023-04-10 14:58:44 -0300

    commit
    Problema JSON parse

--- frontend/src/components/axiosInstances.js
+++ frontend/src/components/axiosInstances.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000", // Definindo a base
+  baseURL: "http://localhost:8000/api", // Definindo a base
 });
 
 export default axiosInstance;
