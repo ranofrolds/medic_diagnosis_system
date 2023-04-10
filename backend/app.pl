ce6eb7dd97deae823055abf043680bd4ecbbaeb7
Pedro Zafalon <71289265+ranofrolds@users.noreply.github.com>
2023-04-10 14:06:41 -0300

    colocando cors back

--- backend/app.pl
+++ backend/app.pl
@@ -1,9 +1,7 @@
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
-:- use_module(library(http/http_cors)).
-:- use_module(library(http/http_json)).
-:- use_module(library(http/http_parameters)).
-:- use_module(library(http/http_error)).
+:- use_module(library(http/html_write)).
+:- use_module(library(http/http_headers)).
 :- use_module(handlers/buscar_cpf).
 :- use_module(handlers/processar_diagnostico).
 :- use_module(handlers/listar_pacientes).
@@ -12,9 +10,6 @@
 
 :- consult('./database/doencas.pl').
 
-% Define a política de CORS
-:- set_setting_default(http:cors, [*]).
-
 % URL handlers.
 % :- http_handler('/buscar_cpf', buscar_cpf_handler, []).
 :- http_handler('/processar_diagnostico', processar_diagnostico_handler, []).
@@ -22,11 +17,34 @@
 :- http_handler('/remover_paciente', remover_paciente_handler, []).
 :- http_handler('/listar_sintomas', listar_sintomas_handler, []).
 
-listar_pacientes_handler(_Request) :-
-    read_file('./database/pacientes.txt', Lines),
-    read_pacientes(Lines, Pacientes),
-    cors_enable(_, [methods([get]), headers([content-type])],
-                reply_json_dict(json{pacientes: Pacientes})).
+% Manipulador de cabeçalho para permitir o CORS
+:- http_handler(root(.), cors_handler, [prefix]).
+
+cors_headers([
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Accept, Authorization'
+]).
+
+cors_handler(Request) :-
+    (   memberchk(origin(Origin), Request) ->
+        cors_headers(Headers),
+        append(Headers, ['Access-Control-Allow-Origin': Origin], CorsHeaders)
+    ;   cors_headers(CorsHeaders)
+    ),
+    (   http_dispatch(Request) ->
+        true
+    ;   format('Content-type: text/plain~n~n'),
+        format('Not found~n'),
+        cors_reply(CorsHeaders)
+    ).
+
+cors_reply(Headers) :-
+    format('Content-type: text/plain~n'),
+    maplist(reply_header, Headers).
+
+reply_header(Key-Value) :-
+    format('~w: ~w~n', [Key, Value]).
 
 server(Port) :-
     http_server(http_dispatch, [port(Port)]),
