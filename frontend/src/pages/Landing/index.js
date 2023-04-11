271a7042373d48e66d63f12bf6aa11482f9d5f04
Gabriel Biscaia <ra118928@uem.br>
2023-04-11 00:12:11 -0300

    ultima atualização para tirar o bug

--- frontend/src/pages/Landing/index.js
+++ frontend/src/pages/Landing/index.js
@@ -1,14 +1,9 @@
 import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
-import { useEffect } from "react";
 
 import "../../styles/style.css";
 
 export const Landing = () => {
-  useEffect(() => {
-    window.location.reload();
-  }, []);
-
   return (
     <div className="main-div">
       <div className="box-div">
