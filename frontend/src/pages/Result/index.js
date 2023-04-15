302240279f8aee5a24ff1d58b166dcfba2542929
Gabriel Biscaia <ra118928@uem.br>
2023-04-15 00:49:45 -0300

    atualização da lógica do result

--- frontend/src/pages/Result/index.js
+++ frontend/src/pages/Result/index.js
@@ -4,7 +4,7 @@ import Doenca from "../../components/Doenca";
 import axiosInstance from "../../components/axiosInstances";
 import somarArray from "../../components/concatenacaoDados";
 import formatarDados from "../../components/formatarDados";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 import "../../styles/style.css";
 
@@ -13,36 +13,60 @@ export const Result = () => {
   const [arrayIndividual, setArrayIndividual] = useState([]);
   const [sintomas, setSintomas] = useState();
 
-  useEffect(() => {
-    const aux = somarArray("");
-    const objeto = formatarDados(aux);
-    setSintomas(objeto.sintomas);
-    const url = "/processar_diagnostico";
-    axiosInstance
-      .post(url, objeto)
-      .then((resposta) => {
-        const arrayDoencas = Object.entries(resposta.data);
+  const findCpfInJson = (data, cpf) => {
+    data.pacientes.forEach((paciente) => {
+      if (paciente.cpf === cpf) {
+        return true;
+      }
+    });
+    return false;
+  };
 
-        const probabilidadesGerais = arrayDoencas.find(
-          (item) => item[0] === "probabilidadesGerais"
-        )[1];
-        setArrayGeral(
-          Object.entries(probabilidadesGerais).sort(([, v1], [, v2]) => v2 - v1)
-        );
+  const aux = somarArray("");
+  const objeto = formatarDados(aux);
+  setSintomas(objeto.sintomas);
+  const cpf = objeto.cpf;
 
-        const probabilidadesIndividuais = arrayDoencas.find(
-          (item) => item[0] === "probabilidadesIndividuais"
-        )[1];
-        setArrayIndividual(
-          Object.entries(probabilidadesIndividuais).sort(
-            ([, v1], [, v2]) => v2 - v1
-          )
-        );
-      })
-      .catch((erro) => {
-        console.error("Erro ao enviar objeto:", erro.message);
-      });
-  }, []);
+  axiosInstance
+    .post("/listar_pacientes")
+    .then((resposta) => {
+      const checkCpf = findCpfInJson(resposta.data, cpf);
+
+      console.log("findCpf: ", checkCpf);
+
+      if (!findCpfInJson) {
+        const url = "/processar_diagnostico";
+        axiosInstance
+          .post(url, objeto)
+          .then((resposta) => {
+            const arrayDoencas = Object.entries(resposta.data);
+
+            const probabilidadesGerais = arrayDoencas.find(
+              (item) => item[0] === "probabilidadesGerais"
+            )[1];
+            setArrayGeral(
+              Object.entries(probabilidadesGerais).sort(
+                ([, v1], [, v2]) => v2 - v1
+              )
+            );
+
+            const probabilidadesIndividuais = arrayDoencas.find(
+              (item) => item[0] === "probabilidadesIndividuais"
+            )[1];
+            setArrayIndividual(
+              Object.entries(probabilidadesIndividuais).sort(
+                ([, v1], [, v2]) => v2 - v1
+              )
+            );
+          })
+          .catch((erro) => {
+            console.error("Erro ao enviar objeto:", erro.message);
+          });
+      }
+    })
+    .catch((erro) => {
+      console.error("Erro ao enviar objeto:", erro.message);
+    });
 
   return (
     <div className="main-div">
